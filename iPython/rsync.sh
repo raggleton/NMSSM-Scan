773539b1ad2d271a701644482f62ede72fb601bc
Robin Aggleton <robinaggleton@gmail.com>
2015-05-15 17:58:10 +0200

    Exclude CSV from rsync

--- iPython/rsync.sh
+++ iPython/rsync.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Synchronise this folder with Dropbox, leaving out big hdf5 files
-rsync -avzP --exclude=*.h5 ~/NMSSM-Scan/iPython/ ~/Dropbox/NMSSM-Scan/iPython/
+rsync -avzP --exclude=*.csv --exclude=*.h5 ~/NMSSM-Scan/iPython/ ~/Dropbox/NMSSM-Scan/iPython/
