0f8167fd3db18b90d5ace006a8a64094e0cd8cfa
Robin Aggleton <robin.aggleton@cern.ch>
2015-05-27 14:50:29 +0100

    Utilise handy csv.DictReader so I don't have to make it myself

--- daniele_points/read_daniele_csv.py
+++ daniele_points/read_daniele_csv.py
@@ -10,10 +10,7 @@ import re
 
 
 with open('ma1_10_br.dat') as csvfile:
-    reader = csv.reader(csvfile, delimiter=' ')
-
-    headings = reader.next()
-    print headings
+    reader = csv.DictReader(csvfile, delimiter=' ')
 
     # make a new card for each parameter point
     hdfs_dir = "/hdfs/user/ra12451/NMSSM-Scan/daniele_points/"
@@ -24,21 +21,16 @@ with open('ma1_10_br.dat') as csvfile:
         inp_proto_lines = inp.readlines()
 
     for i, line in enumerate(reader):
-        if i > 10:
-            break
-        lineDict = dict(zip(headings, line))
-
-        # put input card on hdfs
-        # and replace the necessary fields:
+        # put input cards on hdfs and replace the necessary fields:
         # tan beta, lambda, kappa, alambda, akappa, mueff
         output_file = hdfs_dir + "inp_%d.dat" % i
         with open(output_file, 'w')as output:
             for inp_line in inp_proto_lines:
-                new_inp_line = re.sub(r'SED_TGBETA', lineDict['tb'], inp_line)
-                new_inp_line = re.sub(r'SED_LAMBDA', lineDict['lambda'], new_inp_line)
-                new_inp_line = re.sub(r'SED_KAPPA', lineDict['kappa'], new_inp_line)
-                new_inp_line = re.sub(r'SED_ALAMBDA', lineDict['alambda'], new_inp_line)
-                new_inp_line = re.sub(r'SED_AKAPPA', lineDict['akappa'], new_inp_line)
-                new_inp_line = re.sub(r'SED_MUEFF', lineDict['mueff'], new_inp_line)
+                new_inp_line = re.sub(r'SED_TGBETA', line['tb'], inp_line)
+                new_inp_line = re.sub(r'SED_LAMBDA', line['lambda'], new_inp_line)
+                new_inp_line = re.sub(r'SED_KAPPA', line['kappa'], new_inp_line)
+                new_inp_line = re.sub(r'SED_ALAMBDA', line['alambda'], new_inp_line)
+                new_inp_line = re.sub(r'SED_AKAPPA', line['akappa'], new_inp_line)
+                new_inp_line = re.sub(r'SED_MUEFF', line['mueff'], new_inp_line)
                 output.write(new_inp_line)
         print output_file
