840a1e2443071cf1c0c0e77d1f289c4e95dd497f
Robin Aggleton <robin.aggleton@cern.ch>
2015-05-27 21:36:53 +0100

    Make read_daniele into condor jobs

--- daniele_points/read_daniele_csv.py
+++ daniele_points/read_daniele_csv.py
@@ -4,33 +4,45 @@ version of NMSSMTools and check more things.
 """
 
 
+import sys
 import csv
 # import shutil
 import re
 
 
-with open('ma1_10_br.dat') as csvfile:
-    reader = csv.DictReader(csvfile, delimiter=' ')
+def main(start, end):
+    print start, end
+    with open('/hdfs/user/ra12451/NMSSM-Scan/daniele_points/ma1_10_br.dat') as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=' ')
+        # make a new card for each parameter point
+        hdfs_dir = "/hdfs/user/ra12451/NMSSM-Scan/daniele_points/"
+        input_proto = "/hdfs/user/ra12451/NMSSM-Scan/daniele_points/input_PROTO_daniele.dat"
 
-    # make a new card for each parameter point
-    hdfs_dir = "/hdfs/user/ra12451/NMSSM-Scan/daniele_points/"
-    input_proto = "/users/ra12451/NMSSM-Scan/daniele_points/input_PROTO_daniele.dat"
+        # read in input prototype card
+        with open(input_proto, 'r') as inp:
+            inp_proto_lines = inp.readlines()
 
-    # read in input prototype card
-    with open(input_proto, 'r') as inp:
-        inp_proto_lines = inp.readlines()
+        for i, line in enumerate(reader):
+            if i < int(start):
+                continue
+            elif i > int(end):
+                break
 
-    for i, line in enumerate(reader):
-        # put input cards on hdfs and replace the necessary fields:
-        # tan beta, lambda, kappa, alambda, akappa, mueff
-        output_file = hdfs_dir + "inp_%d.dat" % i
-        with open(output_file, 'w')as output:
-            for inp_line in inp_proto_lines:
-                new_inp_line = re.sub(r'SED_TGBETA', line['tb'], inp_line)
-                new_inp_line = re.sub(r'SED_LAMBDA', line['lambda'], new_inp_line)
-                new_inp_line = re.sub(r'SED_KAPPA', line['kappa'], new_inp_line)
-                new_inp_line = re.sub(r'SED_ALAMBDA', line['alambda'], new_inp_line)
-                new_inp_line = re.sub(r'SED_AKAPPA', line['akappa'], new_inp_line)
-                new_inp_line = re.sub(r'SED_MUEFF', line['mueff'], new_inp_line)
-                output.write(new_inp_line)
-        print output_file
+            # put input cards on hdfs and replace the necessary fields:
+            # tan beta, lambda, kappa, alambda, akappa, mueff
+            output_file = hdfs_dir + "inp_%d.dat" % i
+            with open(output_file, 'w')as output:
+                for inp_line in inp_proto_lines:
+                    new_inp_line = re.sub(r'SED_TGBETA', line['tb'], inp_line)
+                    new_inp_line = re.sub(r'SED_LAMBDA', line['lambda'], new_inp_line)
+                    new_inp_line = re.sub(r'SED_KAPPA', line['kappa'], new_inp_line)
+                    new_inp_line = re.sub(r'SED_ALAMBDA', line['alambda'], new_inp_line)
+                    new_inp_line = re.sub(r'SED_AKAPPA', line['akappa'], new_inp_line)
+                    new_inp_line = re.sub(r'SED_MUEFF', line['mueff'], new_inp_line)
+                    output.write(new_inp_line)
+            if i < 10 or i % 500 == 0:
+                print output_file
+
+
+if __name__ == "__main__":
+    main(start=sys.argv[1], end=sys.argv[2])
---
+++ daniele_points/setupRun.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# Special version to do daniele scan poitns - already have input cards
+host=`hostname`
+echo "Running on $host"
+echo "With parameters: $@"
+# parameters are: [job dir to put output] [batch number]
+jobdir=$1
+batchNum=$2
+
+###################
+# Setup NMSSMTools
+###################
+tar -xvzf NMSSMTools_*.tgz
+cd NMSSMTools_*
+# patch bug in moving output files due to relpaths eurgh
+patch run < ../run.patch
+make init
+make
+
+###################
+# Run NMSSMTools over parameter points
+# Do a batch of 5000
+###################
+start=$(($batchNum * 5000))
+limit=$(((($batchNum+1)*5000)-1))
+python /hdfs/user/ra12451/NMSSM-Scan/daniele_points/read_daniele_csv.py $start $limit
+for ((a=$start; a <= $limit ; a++))
+do
+    ./run /hdfs/user/ra12451/NMSSM-Scan/daniele_points/inp_$a.dat
+    rm /hdfs/user/ra12451/NMSSM-Scan/daniele_points/inp_$a.dat
+done
+ls
+
+###################
+# Zip up files to transfer back
+###################
+# tar -cvzf /hdfs/users/ra12451/NMSSM-scan/daniele_points/spectr$batchNum.tgz spectr*.dat paramRange.txt NMSSM_scan.pl
+# tar -cvzf /hdfs/users/ra12451/NMSSM-scan/daniele_points/omega$batchNum.tgz omega*.dat
+
+###################
+# Tidy up
+###################
+# rm spectr*.dat
+# rm omega*.dat
+rm NMSSMTools_*.tgz
+rm -r NMSSMTools_*
