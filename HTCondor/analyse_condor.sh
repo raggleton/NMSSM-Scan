c4bc593ea0d6c67f54f1483a1e7950a149970ff5
Robin Aggleton <robin.aggleton@cern.ch>
2016-04-26 23:00:22 +0100

    Add new condor scripts for making CSV files

--- HTCondor/analyse_condor.sh
+++ HTCondor/analyse_condor.sh
@@ -1,53 +1,71 @@
-#!/bin/bash
+#!/bin/bash -e
+
+source activate ra12451_pandas
 
 # For running on HTcondor
-# ASSUMES THAT THE DIR EXISTS ON /hdfs
+# ASSUMES THAT THE DIRECTORY WITH SPECTRUM FILES EXISTS ON /hdfs
 # Args: <dir with spectrum files> <process ID for uniqueness>
+SPECTRDIR="$1"
+PID="$2"
 
-hdfsdir=/hdfs/user/ra12451/NMSSM-Scan/$1
+# Check if the directory on hdfs exists
+# -----------------------------------------------------------------------------
+hdfsdir=/hdfs/user/${LOGNAME}/NMSSM-Scan/"$SPECTRDIR"
 if [ ! -d $hdfsdir ]; then
     echo "Cannot find dir $hdfsdir"
-else
-    # Make a dir on the worker node with the same name
-    # Extract all files into that new dir
-    if [ ! -d $1 ]; then
-        mkdir $1
-    fi
-    tar -xvzf $hdfsdir/spectr$2.tgz -C $1
-
-    # Check if superiso output exists. If not, we will have to make them -
-    # for legacy folders before SuperIso was implemented
-    # If you don't want it, comment this part out - the analysis script
-    # will still run fine
-    # if [ ! -e $hdfsdir/superiso$2.tgz ]; then
-    #     echo "Running SuperIso"
-    #     tar -xvzf superiso_v*.tgz
-    #     cd superiso*
-    #     make slha
-    #     # Run SuperIso over files and output screen info to file
-    #     for s in ../$1/spectr*.dat;
-    #     do
-    #         siName=${s/spectr/superiso}
-    #         echo $siName
-    #         ./slha.x $s > $siName
-    #     done
-    #     cd ../$1
-    #     tar -cvzf $hdfsdir/superiso$2.tgz superiso*.dat
-    #     cd ..
-    # else
-    #     tar -xvzf $hdfsdir/superiso$2.tgz -C $1
-    # fi
-
-    # Check if nmssmcalc output exists. If so, untar.
-    if [ -e $hdfsdir/nmssmcalc$2.tgz ]; then
-        tar -xvzf $hdfsdir/nmssmcalc$2.tgz -C $1
-    fi
-
-    ls
-
-    # run perl analysis script over files
-    perl Analyse_scans.pl $1 $hdfsdir $2
-
-    # cleanup
-    rm -r $1
+    exit 1
 fi
+
+# Make a dir on the worker node with the same name, extract spectrum files there
+# -----------------------------------------------------------------------------
+if [ ! -d "$SPECTRDIR" ]; then
+    mkdir "$SPECTRDIR"
+fi
+tar -xvzf $hdfsdir/spectr"$PID".tgz -C "$SPECTRDIR"
+
+# Check if superiso output exists. If so, untar.
+# -----------------------------------------------------------------------------
+# If not, we will have to make them -
+# for legacy folders before SuperIso was implemented
+# If you don't want it, comment this part out - the analysis script
+# will still run fine
+# if [ ! -e $hdfsdir/superiso"$PID".tgz ]; then
+#     echo "Running SuperIso"
+#     tar -xvzf superiso_v*.tgz
+#     cd superiso*
+#     make slha
+#     # Run SuperIso over files and output screen info to file
+#     for s in ../"$SPECTRDIR"/spectr*.dat;
+#     do
+#         siName=${s/spectr/superiso}
+#         echo $siName
+#         ./slha.x $s > $siName
+#     done
+#     cd ../"$SPECTRDIR"
+#     tar -cvzf $hdfsdir/superiso"$PID".tgz superiso*.dat
+#     cd ..
+# else
+#     tar -xvzf $hdfsdir/superiso"$PID".tgz -C "$SPECTRDIR"
+# fi
+
+# Check if nmssmcalc output exists. If so, untar.
+# -----------------------------------------------------------------------------
+# if [ -e $hdfsdir/nmssmcalc"$PID".tgz ]; then
+#     tar -xvzf $hdfsdir/nmssmcalc"$PID".tgz -C "$SPECTRDIR"
+# fi
+
+ls
+
+# Run analysis script over files
+# -----------------------------------------------------------------------------
+python analyse_scans.py "$SPECTRDIR" --ID "$PID"
+
+# Copy files to hdfs
+# -----------------------------------------------------------------------------
+for f in *.csv; do
+    hadoop fs -copyFromLocal -f $f ${hdfsdir#/hdfs}/$(basename $f)
+done
+
+# Cleanup
+# -----------------------------------------------------------------------------
+rm -r $SPECTRDIR
---
+++ submit_analysis_condor_new.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+
+"""
+This script submits a new set of analysis jobs on HTCondor. These will parse
+the spectrum files, and output them as CSV files.
+
+This will use a job directory both here and on HDFS, and submit jobs as a
+DAG for ease of monitoring/maintaining.
+"""
+
+
+import os
+import sys
+from glob import glob
+import htcondenser as ht
+
+
+ODIR = '/hdfs/user/%s/NMSSM-Scan/' % os.environ['LOGNAME']
+
+STORAGE_DIR = "/storage/%s/NMSSM-Scan/" % (os.environ['LOGNAME'])
+
+
+def submit_all_analyses(job_dirs, storage_dir, hdfs_dir):
+    """Submit all CSV-making jobs, with a DAG for each entry in job_dirs.
+    Each job within a DAG is for 1 spectr*.tgz
+
+    Probably could be designed better. Paths rely on many assumptions.
+    """
+
+    common_input_files = ['analyse_scans.py', 'NMSSMToolsFields.py',
+                          'SuperIsoFields.py', 'NMSSMCalcFields.py']
+
+    log_stem = 'analysis.$(cluster).$(process)'
+
+    for jdir in job_dirs:
+        jdir = jdir.strip('/')
+        log_dir = os.path.join(storage_dir, jdir, 'logs')
+
+        analysis_jobset = ht.JobSet(exe='HTCondor/analyse_condor.sh',
+                                    copy_exe=True,
+                                    setup_script=None,
+                                    filename=os.path.join(storage_dir, jdir, 'analysis.condor'),
+                                    out_dir=log_dir, out_file=log_stem + '.out',
+                                    err_dir=log_dir, err_file=log_stem + '.err',
+                                    log_dir=log_dir, log_file=log_stem + '.log',
+                                    share_exe_setup=True,
+                                    common_input_files=common_input_files,
+                                    transfer_hdfs_input=False,
+                                    hdfs_store=os.path.join(hdfs_dir, jdir),
+                                    memory='500MB', disk='2GB')
+
+        analysis_dag = ht.DAGMan(filename=os.path.join(storage_dir, jdir, 'analysis.dag'),
+                                 status_file=os.path.join(storage_dir, jdir, 'analysis.status'))
+
+        # add a job to analyse each spectr*.tgz
+        num_tar = len(glob(os.path.join(hdfs_dir, jdir, 'spectr*tgz')))
+        for ind in range(num_tar):
+            job = ht.Job(name='%d_%s_analysis' % (ind, jdir.strip('/')),
+                         args=[jdir, str(ind)],
+                         hdfs_mirror_dir=os.path.join(hdfs_dir, jdir))
+            analysis_jobset.add_job(job)
+            analysis_dag.add_job(job)
+
+        analysis_dag.submit()
+
+        print 'Check status with:'
+        print 'DAGstatus.py', analysis_dag.status_file
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(submit_all_analyses(sys.argv[1:], STORAGE_DIR, ODIR))
