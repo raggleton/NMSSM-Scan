b553e1819ba19171c8fed60ba4334e3543a02daf
Robin Aggleton <robin.aggleton@cern.ch>
2016-04-27 15:01:07 +0100

    Increase job resource requirements

--- submit_scan_condor_new.py
+++ submit_scan_condor_new.py
@@ -87,7 +87,7 @@ def submit_scans(num_jobs, num_points, job_description, card, param_range, stora
                             common_input_files=common_input_files,
                             transfer_hdfs_input=False,
                             hdfs_store=hdfs_store,
-                            memory='500MB', disk='2GB')
+                            memory='1GB', disk='7GB')
 
     scan_dag = ht.DAGMan(filename=os.path.join(storage_dir, job_dir, 'scan.dag'),
                          status_file=os.path.join(storage_dir, job_dir, 'scan.status'))
