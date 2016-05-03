f6cd213abd9bd52e0d00e84d9b687d7f6cb54aa6
Robin Aggleton <robin.aggleton@cern.ch>
2016-05-03 14:31:18 +0100

    Get rid of old condor scripts for make_hdf5

--- iPython/make_hdf5.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# This script is for the condor worker node
-# to run make_hdf5.py
-
-export PATH=/software/miniconda/envs/pandas/bin:/software/miniconda/bin:$PATH
-
-source activate ra12451_pandas
-echo "Running with args: "$@
-python make_hdf5.py $@
