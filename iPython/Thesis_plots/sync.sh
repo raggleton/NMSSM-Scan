b031376b0e54b500419ef04618cbe1d54d7e639f
Robin Aggleton <robin.aggleton@cern.ch>
2016-07-01 15:59:59 +0100

    Add more thesis plots, and fix sync script from exiting after first fails comparison

--- iPython/Thesis_plots/sync.sh
+++ iPython/Thesis_plots/sync.sh
@@ -1,15 +1,21 @@
-#!/bin/bash -e
+#!/bin/bash
 
-for f in *.pdf; do
+for f in *.p*; do
+    echo $f
     dest="/Users/robina/Dropbox/Thesis/thesis/Phenomenology/Figs/Scans/$f"
+
     if [ ! -e "$dest" ]; then
+        echo "Copying $f"
         cp "$f" "$dest"
-    fi
-    # need to remove and copy to make automator run its compression workflow
-    cmp --silent "$f" "/Users/robina/Dropbox/Thesis/thesis/Phenomenology/Figs/Scans/$f"
-    if [ "$?" -ne "0" ]; then
-        echo "Replacing $f"
-        rm /Users/robina/Dropbox/Thesis/thesis/Phenomenology/Figs/Scans/$f
-        cp "$f" "$dest"
+    else
+        # need to remove and copy to make automator run its compression workflow
+        cmp --silent "$f" "$dest"
+        if [ "$?" -ne "0" ]; then
+            echo "Replacing $f"
+            rm "$dest"
+            cp "$f" "$dest"
+        else
+            echo "Not replacing $f"
+        fi
     fi
 done
