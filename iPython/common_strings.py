d669d4a6871e4e85c1c8260591dae227386e8aa9
Robin Aggleton <robin.aggleton@cern.ch>
2016-07-12 16:39:35 +0100

    Add file with common (Tex) strings

---
+++ iPython/common_strings.py
@@ -0,0 +1,46 @@
+M_A1_STR = r"$m_{a_1}\ \mathrm{[GeV]}$"
+M_H1_STR = r"$m_{h_1}\ \mathrm{[GeV]}$"
+M_H2_STR = r"$m_{h_2}\ \mathrm{[GeV]}$"
+
+DEL_A_MU_STR = r"$\Delta a_{\mu}$"
+OMEGA_STR = r"$\Omega h^2$"
+BSMUMU_STR = r"$BR(B_S \to \mu\mu)$"
+RD_STR = r"$R(D)$"
+RDS_STR = r"$R(D^*)$"
+
+TGBETA_STR = r"$\tan\beta$"
+MUEFF_STR = r"$\mu_{eff}\ \mathrm{[GeV]}$"
+LAMBDA_STR = r"$\lambda$"
+KAPPA_STR = r"$\kappa$"
+AKAPPA_STR = r"$A_{\kappa}\ \mathrm{[GeV]}$"
+ALAMBDA_STR = r"$A_{\lambda}\ \mathrm{[GeV]}$"
+
+DEL_A_MU_LIM = [8.77E-10, 4.61E-9]
+OMEGA_LIM = [0.107, 0.131]
+BSMUMU_LIM = [1.72E-9, 4.5E-9]
+RD_LIM = [2.91E-1, 4.91E-1]
+RDS_LIM = [2.78E-1, 3.66E-1]
+
+XSEC_BR_Hi_2A1_4TAU_STR = r'$\sigma\ \times\ BR\ (h_i\ \to\ 2a_1\ \to\ 4\tau)\ \mathrm{[pb]}$'
+XSEC_BR_H1_2A1_4TAU_STR = r'$\sigma\ \times\ BR\ (h_1\ \to\ 2a_1\ \to\ 4\tau)\ \mathrm{[pb]}$'
+XSEC_BR_H2_2A1_4TAU_STR = r'$\sigma\ \times\ BR\ (h_2\ \to\ 2a_1\ \to\ 4\tau)\ \mathrm{[pb]}$'
+
+BR_Hi_2A1_4TAU_STR = r'$\sigma\ \times\ BR\ (h_i\ \to\ 2a_1\ \to\ 4\tau)\ /\ \sigma_{SM}$'
+BR_H1_2A1_4TAU_STR = r'$\sigma\ \times\ BR\ (h_1\ \to\ 2a_1\ \to\ 4\tau)\ /\ \sigma_{SM}$'
+BR_H2_2A1_4TAU_STR = r'$\sigma\ \times\ BR\ (h_2\ \to\ 2a_1\ \to\ 4\tau)\ /\ \sigma_{SM}$'
+
+XSEC_BR_Hi_2A1_4MU_STR = r'$\sigma\ \times\ BR\ (h_i\ \to\ 2a_1\ \to\ 4\mu)\ \mathrm{[pb]}$'
+XSEC_BR_H1_2A1_4MU_STR = r'$\sigma\ \times\ BR\ (h_1\ \to\ 2a_1\ \to\ 4\mu)\ \mathrm{[pb]}$'
+XSEC_BR_H2_2A1_4MU_STR = r'$\sigma\ \times\ BR\ (h_2\ \to\ 2a_1\ \to\ 4\mu)\ \mathrm{[pb]}$'
+
+BR_Hi_2A1_4MU_STR = r'$\sigma\ \times\ BR\ (h_i\ \to\ 2a_1\ \to\ 4\mu)\ /\ \sigma_{SM}$'
+BR_H1_2A1_4MU_STR = r'$\sigma\ \times\ BR\ (h_1\ \to\ 2a_1\ \to\ 4\mu)\ /\ \sigma_{SM}$'
+BR_H2_2A1_4MU_STR = r'$\sigma\ \times\ BR\ (h_2\ \to\ 2a_1\ \to\ 4\mu)\ /\ \sigma_{SM}$'
+
+XSEC_BR_Hi_2A1_2TAU2MU_STR = r'$\sigma\ \times\ BR\ (h_i\ \to\ 2a_1\ \to\ 2\tau 2\mu)\ \mathrm{[pb]}$'
+XSEC_BR_H1_2A1_2TAU2MU_STR = r'$\sigma\ \times\ BR\ (h_1\ \to\ 2a_1\ \to\ 2\tau 2\mu)\ \mathrm{[pb]}$'
+XSEC_BR_H2_2A1_2TAU2MU_STR = r'$\sigma\ \times\ BR\ (h_2\ \to\ 2a_1\ \to\ 2\tau 2\mu)\ \mathrm{[pb]}$'
+
+BR_Hi_2A1_2TAU2MU_STR = r'$\sigma\ \times\ BR\ (h_i\ \to\ 2a_1\ \to\ 2\tau 2\mu)\ /\ \sigma_{SM}$'
+BR_H1_2A1_2TAU2MU_STR = r'$\sigma\ \times\ BR\ (h_1\ \to\ 2a_1\ \to\ 2\tau 2\mu)\ /\ \sigma_{SM}$'
+BR_H2_2A1_2TAU2MU_STR = r'$\sigma\ \times\ BR\ (h_2\ \to\ 2a_1\ \to\ 2\tau 2\mu)\ /\ \sigma_{SM}$'
