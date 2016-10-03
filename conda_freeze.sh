e1384a546904feaee85fb564e0bcdaa03932d883
Robin Aggleton <robin.aggleton@cern.ch>
2016-10-03 12:06:05 +0100

    Change conda settings filenames

--- conda_freeze.sh
+++ conda_freeze.sh
@@ -1,4 +1,8 @@
 #!/bin/bash -e
-conda list --export > conda_pandas.txt
-conda env export > conda_pandas.yml
+
+# Use the yaml: conda env create -f conda_scans.yml
+conda env export > conda_scans.yml
+
+# Use the txt: conda create --name scans --file conda_scnas.txt
+# conda list --export > conda_scans.txt
 
